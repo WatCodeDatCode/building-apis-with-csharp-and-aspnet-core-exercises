2205b8d6871466ead116bc11a3694b67e672b899
Andrew Russell <arussell742@gmail.com>
2025-03-26 12:29:54 +0100

    update app and add SeedData

--- 2-C-writing-our-first-tests/TheEmployeeAPI/Program.cs
+++ 2-C-writing-our-first-tests/TheEmployeeAPI/Program.cs
@@ -1,61 +1,38 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using TheEmployeeAPI.Abstractions;
 using FluentValidation;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TheEmployeeAPI;
-using TheEmployeeAPI.Abstractions;
-using TheEmployeeAPI.Employees;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var employees = new List<Employee>
-{
-    new Employee { 
-        Id = 1, 
-        FirstName = "John", 
-        LastName = "Doe", 
-        Benefits = new List<EmployeeBenefits> { 
-            new EmployeeBenefits { BenefitType = BenefitType.Health, Cost = 100},
-            new EmployeeBenefits { BenefitType = BenefitType.Dental, Cost = 50 }
-        }
-    },
-        new Employee { 
-        Id = 2, 
-        FirstName = "Bobby", 
-        LastName = "Brown", 
-        Benefits = new List<EmployeeBenefits> { 
-            new EmployeeBenefits { BenefitType = BenefitType.Dental, Cost = 50 }
-        }
-    }
-};
-
-var employeeRepository = new EmployeeRepository();
-foreach (var e in employees) {
-    employeeRepository.Create(e);
-}
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen(options =>
 {
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "TheEmployeeAPI.xml"));
 });
-
-builder.Services.AddSingleton<IRepository<Employee>>(employeeRepository); // Added as singleton because in memory, when working with real DB this will not be the case
+builder.Services.AddSingleton<IRepository<Employee>, EmployeeRepository>(); // Added as singleton because in memory, when working with real DB this will not be the case
 builder.Services.AddProblemDetails();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-builder.Services.AddControllers(options =>
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers(options => 
 {
     options.Filters.Add<FluentValidationFilter>();
 });
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddDbContext<AppDbContext>(options => {
-    options.UseSqlite("Data Source=employees.db");
-});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Seed(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -64,8 +41,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapControllers();
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
 
